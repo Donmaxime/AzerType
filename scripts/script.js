19b3052167d5d6cdaeccea778ab5733f183ab58c
Donmaxime <aledimaxime@gmail.com>
2025-11-13 22:16:06 +0000

    Fonctionnalités de base terminées

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,7 @@
  *********************************************************************************/
 
 /**
- * Cette fonction affiche dans la console le score de l'utilisateur
+ * Cette fonction affiche le score de l'utilisateur
  * @param {number} score : le score de l'utilisateur
  * @param {number} nbMotsProposes : le nombre de mots proposés à l'utilisateur
  */
@@ -19,13 +19,17 @@ function afficherResultat(score, nbMotsProposes) {
     spanScore.innerText = affichageScore
 }
 
+/**
+ * Fonction qui s'ocuupe de donner les propositions à saisir aux joueurs
+ * @param {string} proposition 
+ */
 function afficherProposition(proposition) {
     let zoneProposition = document.querySelector(".zoneProposition")
     zoneProposition.innerText = proposition
 }
 
 /**
- * 
+ * Fonction de partage de nos stats à un ami par e-mail
  * @param {string} nom 
  * @param {string} email 
  * @param {string} score 
@@ -33,6 +37,64 @@ function afficherProposition(proposition) {
 function afficherEmail(nom, email, score) {
     let mailto = `mailto:${email}?subject=Partage du score Azertype&body=Salut, je suis ${nom} et je viens de réaliser le score ${score} sur le site d'Azertype !`
     location.href = mailto
+}
+
+/**
+ * Fonction de validation du nom
+ * @param {string} nom 
+ * @throws {Error}
+ */
+function validerNom(nom) {
+    if (nom.length < 2) {
+        throw new Error("Le nom est trop court.")
+    }
+}
+
+/**
+ * Fonction de valdation de l'email
+ * @param {string} email 
+ * @throws {Error}
+ */
+function validerEmail(email) {
+    let emailRegExp = new RegExp("[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\\.[a-zA-Z0-9._-]+")
+    if (!emailRegExp.test(email)) {
+        throw new Error("L'e-mail n'est pas valide.")
+    } 
+}
+
+/**
+ * Fonction qui affiche un message d'erreur dans la popup en fonction de la nature de l'erreur
+ * @param {string} message 
+ */
+function afficherMessageErreur(message) {
+    let spanErreurMessage = document.getElementById("erreurMessage")
+
+    if (!spanErreurMessage) {
+        let popup = document.querySelector(".popup")
+        spanErreurMessage = document.createElement("span")
+        spanErreurMessage.id = "erreurMessage"
+        popup.append(spanErreurMessage)
+    }
+
+    spanErreurMessage.innerText = message
+}
+
+/**
+ * Fonction de gestion du formulaire de la popup
+ * @param {number} score 
+ */
+function gererFormulaire(score) {
+    try {
+    let nom = document.getElementById("nom").value
+    validerNom(nom)
+
+    let email = document.getElementById("email").value
+    validerEmail(email)
+    afficherMessageErreur("")
+    afficherEmail(nom, email, score)
+    } catch(erreur) {
+        afficherMessageErreur(erreur.message)
+    }
 }
 
 /**
@@ -80,14 +142,10 @@ function lancerJeu() {
     }
 
     let form = document.querySelector("form")
-    form.addEventListener("submit", (event) => {
-        event.preventDefault()
-        let nom = document.getElementById("nom")
-        let email = document.getElementById("email")
-        console.log(nom.value + "/" + email.value)
-
-        afficherEmail(nom, email, score)
-    })
+        form.addEventListener("submit", (event) => {
+            event.preventDefault()
+            gererFormulaire(score)
+        })
 
     afficherResultat(score, i)
 }
